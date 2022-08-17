0fcd419b192310c79342a17866f8f0b707821617
tanveershaikh <myselftanveer@gmail.com>
2022-08-17 17:39:25 +0530

    added to xyz

--- src/test/java/library/xyz.java
+++ src/test/java/library/xyz.java
@@ -4,6 +4,9 @@ public class xyz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("payload");
+		System.out.println("payload");
+		System.out.println("payload");
 
 	}
 
