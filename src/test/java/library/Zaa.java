3907d4024bc5d2d86f0296017de2e544b686d761
myselftanveer <myselftanveer@gmail.com>
2022-08-31 17:21:35 +0530

    deleted

--- src/test/java/library/Zaa.java
+++ src/test/java/library/Zaa.java
@@ -3,20 +3,15 @@ package library;
 import org.testng.annotations.Test;
 
 public class Zaa {
-	
+
 	@Test(priority = 0)
 	public void Demoa() {
 		System.out.println("1st test");
 	}
-	
+
 	@Test(priority = 1)
 	public void Demoa1() {
 		System.out.println("2nd test");
-	}
-	
-	@Test(priority = 2)
-	public void Demoa2() {
-		System.out.println("3rd test");
 	}
 
 }
